18b86e445c83828a196fbbb1c1c4f6868bd00bc1
Omor Faruk <omorfarukhr@users.noreply.github.com>
2026-09-26 01:24:46 -0500

    Fix MaterialAlertDialog background to DayNight theme and restore persistent top bar theme icon

--- app/src/main/java/com/twinpane/app/MainActivity.kt
+++ app/src/main/java/com/twinpane/app/MainActivity.kt
@@ -908,6 +908,7 @@ class MainActivity : AppCompatActivity() {
         val textColor = ContextCompat.getColor(this, R.color.text)
 
         menu.findItem(R.id.action_home)?.icon?.mutate()?.setTint(textColor)
+        menu.findItem(R.id.action_theme)?.icon?.mutate()?.setTint(textColor)
         menu.findItem(R.id.action_main_menu)?.icon?.mutate()?.setTint(textColor)
 
         menu.findItem(R.id.action_undo)?.let {
@@ -937,6 +938,10 @@ class MainActivity : AppCompatActivity() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         if (item.itemId == R.id.action_home) {
             finish()
+            return true
+        }
+        if (item.itemId == R.id.action_theme) {
+            ThemeManager.showThemeSelectionDialog(this)
             return true
         }
         if (item.itemId == R.id.action_main_menu) {
